4c62aab568e3f04fd5bf53e44dce96785691cfb8
hivesbee <hivesbee@gmail.com>
2024-05-07 00:59:29 +0900

    add baseURL

--- @frontend/nuxt.config.ts
+++ @frontend/nuxt.config.ts
@@ -39,7 +39,8 @@ export default defineNuxtConfig({
         // pages: {
         //   login: '/signin'
         // }
-    }
+    },
+    baseURL: process.env.AUTH_ORIGIN
   },
   // @nuxtjs.svg-sprite
   svgSprite: {
