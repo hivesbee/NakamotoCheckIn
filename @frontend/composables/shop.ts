ae58cd88970c91766c976a94736a14309acb2cd6
hivesbee <hivesbee@gmail.com>
2024-05-12 09:23:09 +0900

    use apiBaseUrl

--- @frontend/composables/shop.ts
+++ @frontend/composables/shop.ts
@@ -1,11 +1,15 @@
-
 export const useShop = defineStore('shop', () => {
+  const runtimeConfig = useRuntimeConfig()
+
   const shops = ref<Shop[]>([])
+  const baseURL = runtimeConfig.public.apiBaseUrl as string
 
   const fetch = async () => {
-    const { data } = await useFetch<Shop[]>('http://localhost:3000/shops')
+    const { data } = await useFetch<Shop[]>('/shops',{
+      baseURL
+    })
 
-    if (!Array.isArray(data.value)) {
+    if (!Array.isArray(data.value) || !data.value) {
       throw new Error ('shops not found')
     }
 
