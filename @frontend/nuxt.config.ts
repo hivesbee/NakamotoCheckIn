a313980da42a582a64ecef752b87edcbadb4e3b4
hivesbee <hivesbee@gmail.com>
2024-05-26 19:45:59 +0900

    add eslint, nitro, pinia

--- @frontend/nuxt.config.ts
+++ @frontend/nuxt.config.ts
@@ -5,9 +5,9 @@ export default defineNuxtConfig({
   },
   components: {
     dirs: [{
-        path: '~/components',
-        ignore: ['pages/**/*.vue'],
-      }
+      path: '~/components',
+      ignore: ['pages/**/*.vue']
+    }
     ]
   },
   modules: [
@@ -16,13 +16,14 @@ export default defineNuxtConfig({
     '@nuxtjs/svg-sprite',
     '@nuxtjs/tailwindcss',
     '@sidebase/nuxt-auth',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@nuxt/eslint'
   ],
   // routeRules: {
   //   '/api/**': {
   //     cors: true,
-  //     proxy: 'http://localhost:3000/**',
-  //     headers: { 'access-control-allow-methods': '*' }
+  //     proxy: 'http://localhost:8000/auth'
+  //     // headers: { 'access-control-allow-methods': '*' }
   //   }
   // },
   runtimeConfig: {
@@ -41,23 +42,45 @@ export default defineNuxtConfig({
     baseURL: process.env.NUXT_AUTH_URL,
     globalAppMiddleware: true,
     provider: {
-        type: 'authjs',
-        // pages: {
-        //   login: '/signin'
-        // }
+      type: 'authjs'
+      // pages: {
+      //   login: '/signin'
+      // }
     }
+  },
+  // eslint
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  },
+  // nitro
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'es2022'
+      }
+    }
+  },
+  // pinia
+  pinia: {
+    storesDirs: ['./stores/**']
   },
   // @nuxtjs.svg-sprite
   svgSprite: {
     input: '~/assets/img/icons/svg',
     output: '~/assets/img/icons/gen/'
   },
-  
+  vite: {
+    build: {
+      target: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15']
+    }
+  },
 
   // development settings
   devtools: { enabled: true },
   devServer: {
-    port: 8000,
+    port: 8000
     // https: {
     //   key: './cert/server.key',
     //   cert: './cert/server.crt'
