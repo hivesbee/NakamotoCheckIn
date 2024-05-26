57624df65349a37e09da0fe8d47af226688647c4
hivesbee <hivesbee@gmail.com>
2024-05-26 19:50:10 +0900

    refactor page

--- @frontend/pages/tracks/index.vue
+++ @frontend/pages/tracks/index.vue
@@ -5,36 +5,11 @@
 </template>
 
 <script setup lang="ts">
-  definePageMeta({
-    layout: 'authenticated'
-  })
+definePageMeta({
+  layout: 'authenticated'
+})
 
-
-  // NOTE: 仮置き 最初に到達するページのため
-  //       将来的にはログイン処理中に実装
-  const { data } = useAuth()
-  const shop = useShopStore()
-  const user = useUserStore()
-  const track = useTrackStore()
-
-  if (!data.value?.user?.email || !data.value.user.name) {
-    throw new Error('authenticated email does not found.')
-  }
-
-  try {
-    
-    await user.fetch(data.value.user.email)
-    if (!user.user.id) {
-      await user.signUp(data.value.user.name, data.value.user.email)
-      throw new Error('registered user does not found.')
-    }
-
-    await shop.fetch()
-    await track.fetch(user.user.id)
-  } catch {
-    // catch error
-  }
-
+const track = useTrackStore()
 </script>
 
 <style scoped>
