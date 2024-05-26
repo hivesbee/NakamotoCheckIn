78d76695477dd85df9ac3b4bcc5eea3bcac56435
hivesbee <hivesbee@gmail.com>
2024-05-26 19:48:58 +0900

    add /login/check

---
+++ @frontend/pages/login/check.vue
@@ -0,0 +1,51 @@
+<template>
+  <nuxt-layout name="unauthenticated">
+    <div class="loginCheck_main">
+      <p class="loginCheck_note">
+        ログインチェック中です
+      </p>
+    </div>
+  </nuxt-layout>
+</template>
+
+<script setup lang="ts">
+const userStore = useUserStore()
+
+try {
+  await useAsyncData('userStore', () => userStore.signIn())
+}
+catch (e) {
+  console.error(e)
+}
+
+if (!userStore.user) {
+  await useAsyncData('userStore', () => userStore.signUp())
+}
+
+try {
+  const trackStore = useTrackStore()
+  const shopStore = useShopStore()
+
+  await useAsyncData('trackStore', () => trackStore.fetch())
+  await useAsyncData('shopStore', () => shopStore.fetch())
+}
+catch (e) {
+  console.error(e)
+}
+
+if (import.meta.client) {
+  navigateTo('/tracks')
+}
+</script>
+
+<style scoped>
+  .loginCheck_main {
+    @apply grow;
+    @apply flex flex-col justify-center items-center;
+  }
+
+  .loginCheck_note {
+    @apply mb-4;
+    @apply text-xs text-center text-slate-500;
+  }
+</style>
